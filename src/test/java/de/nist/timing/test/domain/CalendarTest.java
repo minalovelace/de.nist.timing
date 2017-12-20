442fe3683ca64b8fa18411004d04acd819933731
Stefan Stern <stfn.strn@gmail.com>
2017-12-20 20:44:55 +0100

    Introduce gradle infrastructure

---
+++ src/test/java/de/nist/timing/test/domain/CalendarTest.java
@@ -0,0 +1,15 @@
+package de.nist.timing.test.domain;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+
+public class CalendarTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
